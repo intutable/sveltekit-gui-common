14e9e64253a7f47d3b1131528de1576319ca43bf
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-01-23 12:31:55 +0100

    feat: output panel

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 import LoadingIndicator from "./loadingIndicator/LoadingIndicator.svelte"
+import OutputPanel from "./output/OutputPanel.svelte"
+import { Output, OutputType } from "./output/types"
 
-export { LoadingIndicator }
+export { LoadingIndicator, OutputPanel, Output, OutputType }
---
+++ tests/OutputPanel.svelte.test.ts
@@ -0,0 +1,32 @@
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/svelte"
+import { Output, OutputType } from "../src/lib"
+import OutputPanel from "../src/lib/output/OutputPanel.svelte"
+
+test("OutputPanel displays default message", () => {
+    const { container, getByText } = render(OutputPanel)
+
+    expect(getByText("INFO")).toBeInTheDocument()
+    expect(getByText("No output available")).toBeInTheDocument()
+    expect(container.querySelector('.info')).toBeInTheDocument()
+})
+
+test("OutputPanel displays provided info message", () => {
+    const output = new Output(OutputType.Info, "No output available")
+
+    const { container, getByText } = render(OutputPanel, { output })
+
+    expect(getByText("INFO")).toBeInTheDocument()
+    expect(getByText(output.message)).toBeInTheDocument()
+    expect(container.querySelector('.info')).toBeInTheDocument()
+})
+
+test("OutputPanel displays provided error message", () => {
+    const output = new Output(OutputType.Error, "Unexpected Error")
+
+    const { container, getByText } = render(OutputPanel, { output })
+
+    expect(getByText("ERROR")).toBeInTheDocument()
+    expect(getByText(output.message)).toBeInTheDocument()
+    expect(container.querySelector('.error')).toBeInTheDocument()
+})
