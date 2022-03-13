8a7ddfabdc321c3dc82ecb482729e41e5a904159
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-03-13 16:41:18 +0100

    fix: index.js imports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import LoadingIndicator from "./loadingIndicator/LoadingIndicator.svelte"
 import OutputPanel from "./output/OutputPanel.svelte"
-import { Output, OutputType } from "./output/types"
+import { Output, OutputType } from "./output/types.js"
 
 export { LoadingIndicator, OutputPanel, Output, OutputType }
